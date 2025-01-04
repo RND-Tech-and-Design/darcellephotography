cdb57f02e0937bfbb74906e56ea7fef073867ed8
RyanDrx <Ryan.Dierickx@gmail.com>
2025-01-04 20:46:17 +0000

    refactor: simplify image URL generation logic in utils

--- composables/utils.ts
+++ composables/utils.ts
@@ -2,10 +2,11 @@ import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const urlFor = (source: SanityImageSource) => {
-    const { projectId, dataset } = useSanity().client.config();
-    return projectId && dataset
-        ? imageUrlBuilder({ projectId, dataset }).image(source)
-        : null;
+   // const { projectId, dataset } = useSanity().client.config();
+    return source;
+    // return projectId && dataset
+    //     ? imageUrlBuilder({ projectId, dataset }).image(source)
+    //     : null;
 }
 
 export const shuffleArray = <T,>(array: T[]): T[] => {
